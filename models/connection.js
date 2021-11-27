183f3fb5edec1175425864383bd5878c1b86ff55
cristianocsl <cslcristiano@gmail.com>
2021-11-27 20:41:15 -0300

    montando connection.jsgp

---
+++ models/connection.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+
+const MONGO_DB_URL = `mongodb://${process.env.HOST}:27017/`;
+const DB_NAME = 'StoreManager';
+
+let db = null;
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connection = () => (db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db(DB_NAME);
+      return db;
+    }).catch((err) => {
+      console.error(err.message);
+      process.exit(1);
+    }));
+
+    module.exports = connection;
