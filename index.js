e06e55c81ea31e56db71fd1f7fa042d891eb3cd6
cristianocsl <cslcristiano@gmail.com>
2021-11-27 20:00:41 -0300

    :rocket: Iniciando o projeto backend-wallet! :rocket:gp

---
+++ index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const { PORT } = process.env;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
