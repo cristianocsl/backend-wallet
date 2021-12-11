137ce560374328be4237e7207d9ad3a82151c22c
cristianocsl <cslcristiano@gmail.com>
2021-12-11 10:21:35 -0300

    Refatora createUser() na camada models para que nada seja retornado

--- models/user/createUser.js
+++ models/user/createUser.js
@@ -1,15 +1,8 @@
 const connection = require('../connection');
 
 const createUser = async (userInfos) => {
-  const { firstName, lastName, email } = userInfos;
   const db = await connection();
-  const result = await db.collection('users').insertOne(userInfos); // com userInfos entre chaves, o objeto retornado deixa de vir com a chave _id repetida.
-  return {
-      _id: result.insertedId,
-      firstName,
-      lastName,
-      email,
-  };
+  await db.collection('users').insertOne(userInfos); // com userInfos entre chaves, o objeto retornado deixa de vir com a chave _id repetida.
 };
 
   module.exports = createUser;
