f4fd58b9f94292f1a06b0ca81ba6f3d419e89894
cristianocsl <cslcristiano@gmail.com>
2021-12-11 10:20:28 -0300

    Refatora register() na camada services para que nada seja retornado

--- service/users/register.js
+++ service/users/register.js
@@ -4,14 +4,12 @@ const ERR_MSG_CREATE = { code: 'invalidData', message: 'Email already exists' };
 
 const createUser = async (userInfos) => {
   const { email } = userInfos;
-  
+
   const repetedEmail = await modelUser.findByEmail(email);
 
   if (repetedEmail) return { err: ERR_MSG_CREATE };
 
-  const createdUser = await modelUser.createUser(userInfos);
-
-  return createdUser;
+  await modelUser.createUser(userInfos);
 };
 
 module.exports = createUser;
