04bf39d547a43676f790bbb4dc5264efbb2a573c
cristianocsl <cslcristiano@gmail.com>
2021-12-17 15:07:45 -0300

    criar metodo get em routerUser

--- controllers/users/routerUser.js
+++ controllers/users/routerUser.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { authentication } = require('../../middlewares');
 const { createUser, login } = require('./index');
 
 const routerUser = express.Router({ mergeParams: true });
 
 routerUser.post('/', createUser);
 routerUser.post('/login', login);
+routerUser.get('/info', authentication);
 
 module.exports = routerUser;
