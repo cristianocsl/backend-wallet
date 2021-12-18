865f95bc8644b4f889591811767defc9c136b800
cristianocsl <cslcristiano@gmail.com>
2021-12-18 11:22:56 -0300

    remove getUserInfo() - findByEmail() tem o mesmo resultado

--- models/user/index.js
+++ models/user/index.js
@@ -1,9 +1,7 @@
 const createUser = require('./createUser');
 const findByEmail = require('./findByEmail');
-const getUserInfo = require('./getUserInfo');
 
 module.exports = {
   createUser,
   findByEmail,
-  getUserInfo,
 };
