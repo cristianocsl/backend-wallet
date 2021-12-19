05adca0d3b03dfd173ef90fe8b78b020c7affd88
cristianocsl <cslcristiano@gmail.com>
2021-12-19 02:47:13 -0300

    adiciona classes para lançar mensagens de erro

--- service/users/register.js
+++ service/users/register.js
@@ -7,7 +7,7 @@ const { AppErrors } = ApiError;
 const createUser = async (userInfos) => {
   const { email } = userInfos;
 
-  const repetedEmail = await modelUser.findByEmail(email);
+  const repetedEmail = await modelUser.findUser(email);
 
   if (repetedEmail) return AppErrors(EMAIL_EXISTING);
 
