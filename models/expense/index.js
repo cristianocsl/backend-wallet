0dde0d563de5df1e61219e811b7b196c2a0f2238
cristianocsl <cslcristiano@gmail.com>
2021-12-20 12:15:13 -0300

    cria updateExpense() na camada service

--- models/expense/index.js
+++ models/expense/index.js
@@ -1,7 +1,9 @@
 const createExpense = require('./createExpense');
 const getExpenses = require('./getExpenses');
+const updateExpense = require('./updateExpense');
 
 module.exports = {
   createExpense,
   getExpenses,
+  updateExpense,
 };
