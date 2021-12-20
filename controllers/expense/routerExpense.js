740723af2e5986c688c58bd781b5fe53b2e694c1
cristianocsl <cslcristiano@gmail.com>
2021-12-20 13:31:49 -0300

    adiciona metodo delete e rota /:id em routerExpense

--- controllers/expense/routerExpense.js
+++ controllers/expense/routerExpense.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { authentication } = require('../../middlewares');
-const { createExpense } = require('./index');
+const { createExpense } = require('.');
 const { updateExpense } = require('.');
+const { deleteExpense } = require('.');
 
 const routerExpense = express.Router({ mergeParams: true });
 
 routerExpense.post('/', authentication, createExpense);
 routerExpense.put('/:id', authentication, updateExpense);
+routerExpense.delete('/:id', authentication, deleteExpense);
 
 module.exports = routerExpense;
