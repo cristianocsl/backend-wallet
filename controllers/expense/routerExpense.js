54ddaa02a744165cff6c65b6980bf817a7cdb3aa
cristianocsl <cslcristiano@gmail.com>
2021-12-20 12:30:12 -0300

    adiciona metodo PUT e rota /:id para editar despesa

--- controllers/expense/routerExpense.js
+++ controllers/expense/routerExpense.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { authentication } = require('../../middlewares');
 const { createExpense } = require('./index');
+const { updateExpense } = require('.');
 
 const routerExpense = express.Router({ mergeParams: true });
 
 routerExpense.post('/', authentication, createExpense);
+routerExpense.put('/:id', authentication, updateExpense);
 
 module.exports = routerExpense;
