a4a60d36346fb9bd2317c2b40ac4a3c82fd86c49
cristianocsl <cslcristiano@gmail.com>
2021-12-20 12:49:30 -0300

    implementa deleteExpense() na camada models

--- models/expense/deleteExpense.js
+++ models/expense/deleteExpense.js
@@ -3,7 +3,9 @@ const connection = require('../connection');
 
 const deleteExpense = async (expenseId) => {
   if (!ObjectId.isValid(expenseId)) return null;
-  const expense = await connection().collection('expenses').findOneAndDelete(expenseId);
+  const expense = await connection().collection('expenses').findOneAndDelete(
+    { _id: ObjectId(expenseId) },
+  );
   return expense;
 };
 
