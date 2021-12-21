954dfdf75b28335a7b48dea992f1c76044cd2902
cristianocsl <cslcristiano@gmail.com>
2021-12-21 18:39:45 -0300

    implemente joi de validaçao para expenses

--- validations/validateExpense.js
+++ validations/validateExpense.js
@@ -5,9 +5,6 @@ const ERR_EXPENSE_REG = {
   'date.iso': 'Data no formato inválido', // PROCURAR FORMATO date.iso ou outros
   'string.base': 'Preencha apenas com letras.',
   'string.empty': 'Este campo não pode ser vazio.',
-  'string.min': 'Utilize no mínimo de 8 caracteres.',
-  'string.email': 'Preencha este campo com um email válido.',
-  'string.pattern': 'Preencha com pelo menos: 1 letra maiúscula, 1 número e 1 caractere especial.',
 };
 
 const SCHEMA_EXPENSE = {
