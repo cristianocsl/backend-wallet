6814af9b40ee864545ea2eb981dd639ead824973
cristianocsl <cslcristiano@gmail.com>
2021-12-21 23:14:50 -0300

    converte birthDate para ISODate atraves de new Date()

--- service/users/createUser.js
+++ service/users/createUser.js
@@ -2,17 +2,24 @@ const { findUser } = require('../../models/user');
 const { ApiError } = require('../../error/apiError');
 const { EMAIL_EXISTING } = require('../../error/msgCodeError');
 const { createUser: registerUser } = require('../../models/user');
+const { dateForMongodb } = require('../../geral');
 
 const { AppErrors } = ApiError;
 
 const createUser = async (userInfos) => {
-  const { email } = userInfos;
+  const { email, birthDate: isoDate } = userInfos;
 
   const repetedEmail = await findUser(email);
 
   if (repetedEmail) return AppErrors(EMAIL_EXISTING);
 
-  const user = await registerUser(userInfos);
+  const birthDate = new Date(
+    await dateForMongodb(isoDate),
+  );
+
+  const modifyUserInfo = { ...userInfos, birthDate };
+
+  const user = await registerUser(modifyUserInfo);
 
   return user;
 };
