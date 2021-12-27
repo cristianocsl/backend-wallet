9abef17208e5c11d7dca8b820081b80ef3227072
cristianocsl <cslcristiano@gmail.com>
2021-12-27 18:31:38 -0300

    modificaçoes em connection.js

--- models/connection.js
+++ models/connection.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 // const MONGO_DB_URL = `mongodb://${process.env.HOST}:27017/`;
-const USERNAME = 'cslcristiano:password>@cluster0.r5ij7.mongodb.net';
+const USERNAME = 'cslcristiano:<password>@cluster0.r5ij7.mongodb.net';
 const MONGO_DB_URL = `mongodb+srv://${USERNAME}/myFirstDatabase?retryWrites=true&w=majority`;
 const DB_NAME = 'backendWallet';
 
