e4981c04459fd22ae54ae12af4c4b29287dfecf4
cristianocsl <cslcristiano@gmail.com>
2021-12-28 12:40:11 -0300

    tentativa de deploy com heroku

--- controllers/users/routerUser.js
+++ controllers/users/routerUser.js
@@ -4,7 +4,6 @@ const { createUser, login, getUserInfo } = require('./index');
 
 const routerUser = express.Router({ mergeParams: true });
 
-routerUser.get('/', (_req, res) => res.status(200).json({ message: 'Heroku funcionando!' }));
 routerUser.post('/', createUser);
 routerUser.post('/login', login);
 routerUser.get('/info', authentication, getUserInfo);
