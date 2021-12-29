e47e2cac51a0b99d633242a4240ac70fed7dfd68
cristianocsl <cslcristiano@gmail.com>
2021-12-29 20:34:31 -0300

    tentativa de deploy com heroku

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const root = require('./controllers/root');
 const error = require('./middlewares/error');
 
 const app = express();
-const { PORT } = process.env || 3000;
+const { PORT } = process.env || 3001;
 
 app.use(bodyParser.json());
 
 // console.log(`${__dirname}/dist/`);
 // const distDir = `${__dirname}/dist/`;
 //  app.use(express.static(distDir));
-app.get('/', (_req, res) => res.status(200).json({ message: 'Heroku funcionando!' }));
+// app.get('/', (_req, res) => res.status(200).json({ message: 'Heroku funcionando!' }));
 
 app.use('/', root);
 app.use(error);
