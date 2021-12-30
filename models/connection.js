e472cc2f54d450222afd55fe9ef2198fe5b15b24
cristianocsl <cslcristiano@gmail.com>
2021-12-30 00:24:48 -0300

    tentativa de deploy com heroku

--- models/connection.js
+++ models/connection.js
@@ -5,24 +5,37 @@ require('dotenv').config();
 // const MONGO_DB_URL = `mongodb://${process.env.HOST}:27017/`;
 
 const { MONGO_DB_URL } = process.env;
-const DB_NAME = 'backendWallet';
+// const DB_NAME = 'backendWallet';
 
-let db = null;
+// let db = null;
 
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-const connection = () => (db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => {
-      db = conn.db(DB_NAME);
-      return db;
-    }).catch((err) => {
-      console.error(err.message);
-      process.exit(1);
-    }));
+const connection = new MongoClient(MONGO_DB_URL, OPTIONS);
 
+const bootStrap = async () => {
+  try {
+    connection.connect();
+  } catch (err) {
+    console.log('Connection failed');
+  }
+};
+
+bootStrap();
+
+// const connection = () => (db
+//     ? Promise.resolve(db)
+//     : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+//     .then((conn) => {
+//       db = conn.db(DB_NAME);
+//       return db;
+//     }).catch((err) => {
+//       console.error(err.message);
+//       process.exit(1);
+//     }));
+
+//     module.exports = connection;
     module.exports = connection;
